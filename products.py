63d8c65d5c6657798f9f3ce4654f3bf789aa172a
Bekbolot Tazhibaev <tabekg@gmail.com>
2025-01-20 18:28:34 +0600

    products

--- products.py
+++ products.py
@@ -1,8 +1,5 @@
 import json
 
-from flask import g
-
-from main import app
 from models.category import Category
 from models.product import Product
 from utils import SessionLocal
@@ -10,10 +7,10 @@ from utils import SessionLocal
 db = SessionLocal()
 
 
-def create_category(title, parent=None):
-    category = db.query(Category).filter_by(title=title).first()
+def create_category(title: str, parent=None):
+    category = db.query(Category).filter_by(title=title.strip()).first()
     if not category:
-        category = Category(title=title, parent_id=parent.id if parent else None)
+        category = Category(title=title.strip(), parent_id=parent.id if parent else None)
         db.add(category)
         db.commit()
     return category
@@ -33,18 +30,18 @@ def save_data(data):
     for item in data:
         kategori_a = create_category(item['KategoriA'])
         kategori_b = create_category(item['KategoriB'], parent=kategori_a)
-        kategori_c = (create_category(item['KategoriC'], parent=kategori_b))
+        kategori_c = create_category(item['KategoriC'], parent=kategori_b)
 
         product = Product(
-            title=item['TanimRU'],
+            title=item['TanimRU'].strip(),
             description=None,
             price=None,
             image_path=None,
             quantity=process_quantity(item['Miktar']),
-            article=item['Artikel'],
-            code=item['Kod'],
-            unit=item['Birim'],
-            category_id=kategori_a.id
+            article=item['Artikel'].strip(),
+            code=item['Kod'].strip(),
+            unit=item['Birim'].strip(),
+            category_id=kategori_c.id,
         )
         db.add(product)
 
@@ -52,9 +49,9 @@ def save_data(data):
 
 
 if __name__ == '__main__':
-    with app.app_context():
-        with open('C:/Users/HP/Desktop/data.json', 'r', encoding='utf-8') as file:
-            data = json.load(file)
-        print(f"Загружено {len(data)} продуктов.")
-        save_data(data)
+    with open('./data.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    print(f"Загружено {len(data)} продуктов.")
+    save_data(data)
 
+    db.close()
